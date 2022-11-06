77aaf1e986beac9d39faf0a315ba642b08380bf5
anganandh <anganandh.ravichandran@solitontech.com>
2022-11-06 22:37:06 +0530

    Add Exerceise 2C
    Signed-off-by: anganandh <anganandh.ravichandran@solitontech.com>

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,15 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Filter from "./Filter";
 import Persons from "./Persons";
 import PhoneBookForm from "./PhoneBookForm";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [searchText, setSearchText] = useState("");
 
   const filteredPerson = searchText.length
@@ -17,6 +13,12 @@ const App = () => {
         person.name.toLowerCase().includes(searchText.toLowerCase())
       )
     : persons;
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   const addNewPerson = (e, newName, newNumber) => {
     e.preventDefault();
