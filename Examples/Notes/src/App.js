d9ee89331f02ad2a3cb7633a04591aa6b4c6fa7e
anandh003 <angaravi003@gmail.com>
2022-11-13 18:47:34 +0530

    Add Notificaiton and footer component
    Signed-off-by: anandh003 <angaravi003@gmail.com>

--- Examples/Notes/src/App.js
+++ Examples/Notes/src/App.js
@@ -1,11 +1,15 @@
 import Note from "./components/Note";
 import { useState, useEffect } from "react";
 import noteService from "./services/notes";
+import Notification from "./components/Notification";
+import Footer from "./components/Footer";
+import "./index.css";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     noteService
@@ -44,7 +48,8 @@ const App = () => {
         setNotes(notes.map((note) => (note.id === id ? updatedNote : note)))
       )
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`);
+        setErrorMessage(`Note ${note.content} was already removed from server`);
+        setTimeout(() => setErrorMessage(null), 5000);
         setNotes(notes.filter((note) => note.id !== id));
       });
   };
@@ -54,6 +59,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           {" "}
@@ -78,6 +84,7 @@ const App = () => {
         />
         <button type="submit">Save</button>
       </form>
+      <Footer />
     </div>
   );
 };
