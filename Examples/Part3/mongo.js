edd93881ebed29bc6009cb25b4321a734d19b926
anandh003 <angaravi003@gmail.com>
2023-02-06 07:29:37 +0530

    Update example
    Signed-off-by: anandh003 <angaravi003@gmail.com>

--- Examples/Part3/mongo.js
+++ Examples/Part3/mongo.js
@@ -20,11 +20,18 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model("Note", noteSchema);
 
 const note = new Note({
-  content: "CSS is Hard",
+  content: "GET and POST are the most important methods of HTTP protocol",
   important: true,
 });
 
-note.save().then((result) => {
-  console.log("note saved!");
+// note.save().then((result) => {
+//   console.log("note saved!");
+//   mongoose.connection.close();
+// });
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
   mongoose.connection.close();
 });
