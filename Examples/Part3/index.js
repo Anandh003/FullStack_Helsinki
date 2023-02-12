a2a1f7b4537582e4514619163b241af76c3ee972
anandh003 <angaravi003@gmail.com>
2023-02-12 09:01:19 +0530

    Part-3 tutorial
    Signed-off-by: anandh003 <angaravi003@gmail.com>

--- Examples/Part3/index.js
+++ Examples/Part3/index.js
@@ -1,50 +1,15 @@
-const { request, response } = require("express");
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const Note = require("./models/note");
+const { response } = require("express");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
-
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World</h1>");
-});
-
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
-});
-
-app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
-});
+const PORT = process.env.PORT;
 
 const generateId = () => {
   const maxId =
@@ -52,28 +17,61 @@ const generateId = () => {
   return maxId + 1;
 };
 
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) response.json(note);
+      else response.status(404).end();
+    })
+    .catch((err) => {
+      console.log(err);
+      response.status(400).send({ error: "malformatted id" });
+    });
+});
+
 app.post("/api/notes", (request, response) => {
   if (!request.body.content) {
     return response.status(400).json({ error: "contetnt missing" });
   }
   const body = request.body;
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
+  });
+  note.save().then((savedNote) => response.json(savedNote));
+});
+
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => response.status(204).end())
+    .catch((err) => next(err));
+});
+
+app.put("/api/notes/:id", (request, reponse, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
   };
-  notes = notes.concat(note);
-  response.json(note);
-});
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-  response.status(204).end();
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      debugger;
+      reponse.json(updatedNote);
+    })
+    .catch((err) => next(err));
 });
-
-const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ Examples/Part3/models/note.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(url)
+  .then((result) => console.log("connected to MongoDB"))
+  .catch((err) => console.log("Error connecting to MongoDB", err.message));
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: String,
+});
+
+noteSchema.set("toJSON", {
+  transform: (document, returnObj) => {
+    returnObj.id = returnObj._id.toString();
+    delete returnObj._id;
+    delete returnObj.__v;
+  },
+});
+
+module.exports = mongoose.model("Note", noteSchema);
