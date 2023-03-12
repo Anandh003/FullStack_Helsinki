fa417c595c410f9adbfc242614f856cf4ffcebae
Anandh003 <angaravi003@gmail.com>
2023-03-12 16:49:29 +0530

    Structure bloglist project
    Signed-off-by: Anandh003 <angaravi003@gmail.com>

---
+++ part-4/BlogList/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const blogListRouter = require("./controller/blogList");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => logger.info("Connected to DB"))
+  .catch((err) => logger.error(`Error in DB ${err.message}`));
+
+app.use("/api/blog", blogListRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
